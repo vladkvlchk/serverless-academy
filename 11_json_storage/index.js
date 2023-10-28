a32d1c4dc5a01709d5ea45075471207a25b5996b
Vladyslav <prostoandrei9@gmail.com>
2023-10-28 15:04:13 +0300

    file storage

--- 11_json_storage/index.js
+++ 11_json_storage/index.js
@@ -1,22 +1,33 @@
 const express = require("express");
+const fs = require("fs");
 
 const PORT = 5000;
 
 const app = express();
 app.use(express.json());
 
-const storage = new Map();
-
 app.get("/:json_path", (req, res) => {
-  const data = storage.get(req.params.json_path);
-  if(!data) res.status(404).send("The store is empty. Try another path or post something here.")
-  res.json(data);
+  try {
+    const data = require(`./db/${req.params.json_path}.json`);
+    res.json(data);
+  } catch (e) {
+    res.status(404).send('Empty');
+    console.log(e);
+  }
 });
 
 app.post("/:json_path", (req, res) => {
-  const data = req.body;
-  storage.set(req.params.json_path, data);
-  res.sendStatus(200);
+  try {
+    fs.writeFileSync(
+      `./db/${req.params.json_path}.json`,
+      JSON.stringify(req.body),
+      "utf-8"
+    );
+    res.sendStatus(200);
+  } catch (e) {
+    res.sendStatus(500);
+    console.log(e);
+  }
 });
 
 app.listen(PORT, () => console.log("Server started on port ", PORT));
