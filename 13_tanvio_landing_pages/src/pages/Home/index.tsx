d15cebe3bdcfc7a8067f11c021705a4dd527c59e
Vladyslav <prostoandrei9@gmail.com>
2023-10-31 17:54:06 +0200

    added logo

--- 13_tanvio_landing_pages/src/pages/Home/index.tsx
+++ 13_tanvio_landing_pages/src/pages/Home/index.tsx
@@ -1,9 +1,32 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Home.module.scss";
+import Logo from "../../assets/logo";
 
-const Home : React.FC = () => {
-    return(
-        <div>Home</div>
-    )
-}
+const Home: React.FC = () => {
+  return (
+    <header>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/features">Features</Link>
+          </li>
+          <li>
+            <Link to="/company">Company</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.getStarted}>
+        <button>Get Started</button>
+      </div>
+    </header>
+  );
+};
 
-export default Home
+export default Home;
