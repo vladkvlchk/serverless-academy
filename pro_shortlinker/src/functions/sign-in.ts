91de4f63ff3bdbe0162d82451bd5c1d7e60cf5fb
Vladyslav <prostoandrei9@gmail.com>
2023-11-03 04:06:58 +0200

    connected db

--- pro_shortlinker/src/functions/sign-in.ts
+++ pro_shortlinker/src/functions/sign-in.ts
@@ -1,6 +1,7 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 import * as jwt from "jsonwebtoken";
-import * as bcrypt from "bcrypt";
+import * as bcryptjs from "bcryptjs";
+import * as jose from "node-jose";
 
 import AWS = require("aws-sdk");
 // const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -12,33 +13,58 @@ export const handler = async (
     const body = JSON.parse(event.body);
     const { email, password } = body;
 
+    let token;
+
+    const privateKeyPem = `-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAvMC00W5cBwLgXJ/j5tD6W2PLgES+NC9KhxjZMunDaQrY6UVI\n... ${process.env.PRIVATE_KEY} ...\n-----END RSA PRIVATE KEY-----`;
+    const publicKeyPem = '-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAvMC00W5cBwLgXJ/j5tD6W2PLgES+NC9KhxjZMunDaQrY6UVI\n... 11nsdidi7d9nfnd23b ...\n-----END RSA PUBLIC KEY-----';
+
+
+    await jose.JWK.asKey({
+      kty: 'RSA',
+      n: publicKeyPem, 
+      d: privateKeyPem,
+    }).then((key) => {
+      return jose.JWE.createEncrypt({ format: 'compact' }, key)
+        .update(JSON.stringify({ email }))
+        .final();
+    })
+    .then((jweToken) => {
+      token = jweToken;
+    })
+    .catch((error) => {
+      throw new Error(error.message)
+    });
+
+    // const hashedPassword = await bcryptjs.hash(password, 10); // 10 rounds for hashing
+
     // const accessToken = jwt.sign({ email }, process.env.ACCESS_KEY, {
     //   expiresIn: "1h",
     // });
     // const refreshToken = jwt.sign({ email }, process.env.RESRESH_KEY, {
     //   expiresIn: "30d",
     // });
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-       message: "hello from sign-in function"
-      }),
-    };
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify({
+    //    message: "hello from sign-in function"
+    //   }),
+    // };
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        access_key: process.env.ACCESS_KEY,
-        refresh_key: process.env.REFRESH_KEY,
-        bcrypt_secret: process.env.BCRYPT_SECRET,
-        email,
-        password
+        // access_key: process.env.ACCESS_KEY,
+        // refresh_key: process.env.REFRESH_KEY,
+        // bcrypt_secret: process.env.BCRYPT_SECRET,
+        // email,
+        // password
+        token
       }),
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Internal Server Error" }),
+      body: JSON.stringify({ error: error.message }),
     };
   }
 };
--- pro_shortlinker/src/functions/sign-up.ts
+++ pro_shortlinker/src/functions/sign-up.ts
@@ -1,6 +1,6 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 import * as jwt from 'jsonwebtoken';
-import * as bcrypt from 'bcrypt';
+import * as bcryptjs from 'bcryptjs';
 import * as AWS from 'aws-sdk';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -10,29 +10,29 @@ export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyRe
     const body = JSON.parse(event.body);
     const { email, password } = body;
 
-    const hashedPassword = await bcrypt.hash(password, process.env.BCRYPT_SECRET);
+    const hashedPassword = await bcryptjs.hash(password, 10);
 
     const params = {
       TableName: 'Users',
       Item: {
-        Email: email,
-        Password: hashedPassword,
+        email,
+        password: hashedPassword,
       },
     };
 
     await dynamodb.put(params).promise();
 
-    const accessToken = jwt.sign({ email }, process.env.ACCESS_KEY, { expiresIn: '1h' });
-    const refreshToken = jwt.sign({ email }, process.env.RESRESH_KEY, { expiresIn: '30d' });
+    const accessToken = 0//jwt.sign({ email }, process.env.ACCESS_KEY, { expiresIn: '1h' });
+    const refreshToken = 0//jwt.sign({ email }, process.env.REFRESH_KEY, { expiresIn: '30d' });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ accessToken, refreshToken, hashedPassword }), //REMOVE PASSWORD!!!
+      body: JSON.stringify({ accessToken, refreshToken }), //REMOVE PASSWORD!!!
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
+      body: JSON.stringify({ error: error.message }), 
     };
   }
 };
