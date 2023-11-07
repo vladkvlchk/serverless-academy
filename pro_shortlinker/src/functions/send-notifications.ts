dcabcc2c9dcfaa440169493fb444a07d86a92e4e
Vladyslav <prostoandrei9@gmail.com>
2023-11-07 04:57:03 +0200

    openapi spec

--- pro_shortlinker/src/functions/send-notifications.ts
+++ pro_shortlinker/src/functions/send-notifications.ts
@@ -22,52 +22,34 @@ export const handler = async (event: SQSEvent) => {
 
     console.log(messages_map);
 
-    for (let arr in messages_map.values()) {
-      console.log(arr);
+    await messages_map.forEach(async (value, key, map) => {
+
       const params = {
         Destination: {
-          ToAddresses: ["5710233@stud.nau.edu.ua"], //owner_email
+          ToAddresses: [value.owner_email],
         },
         Message: {
           Body: {
             Text: {
-              Data: arr
+              Data: value
               //@ts-ignore
                 .map((link) => `Your link ${link.short_link} was deactivated`)
                 .join("\n"),
             },
           },
           Subject: {
-            Data: "Pro-shortlinker notifications",
+            Data: "Pro-shortlinker notifications 2.0",
           },
         },
         Source: process.env.SENDER_EMAIL,
       };
 
       await ses.sendEmail(params).promise();
-    }
-
-    const params = {
-      Destination: {
-        ToAddresses: ["5710233@stud.nau.edu.ua"], //owner_email
-      },
-      Message: {
-        Body: {
-          Text: {
-            //@ts-ignore
-            Data: "Notifications were sended",
-          },
-        },
-        Subject: {
-          Data: "Pro-shortlinker notifications",
-        },
-      },
-      Source: process.env.SENDER_EMAIL,
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "emails are sended successfully" }),
     };
-
-    const res = await ses.sendEmail(params).promise();
-
-    console.log(res);
   } catch (err) {
     console.error(err.message);
   }
